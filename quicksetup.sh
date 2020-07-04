b15a82fd98c2fd70776f6c08ed4b88edf9074144
akshaynexus <akshaycm@hotmail.com>
2020-07-05 00:03:53 +0300

    Add cron job to try to update each day

--- quicksetup.sh
+++ quicksetup.sh
@@ -19,6 +19,12 @@ sudo apt-get install python git unzip pv jq dnsutils
 UNATTENDED=1
 bash bin/ghostman.sh install
 bash bin/ghostman.sh restart now
+## Add crontab to try updating each day
+cat > mycron << EOL
+0 0 * * * cd $HOME/ghostman  && bash bin/ghostman.sh update >> ./tmp/update.log 2>&1
+EOL
+crontab mycron
+rm -f mycron
 bash bin/ghostman.sh stakingnode init
 bash bin/ghostman.sh stakingnode new
 
